65bd6534559a821f2a6e3016b29a27d2a07c85ad
alexburnet <alexburnet03@gmail.com>
2024-03-19 17:27:51 +0000

    Initial Service Implementation

--- src/Service.java
+++ src/Service.java
@@ -1,2 +1,77 @@
-public class Service {
+import java.io.*;
+import java.net.Socket;
+
+public class Service extends Thread{
+
+    Socket socket;
+
+    String[] request;
+
+    String returnValue;
+
+    Service(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void interpretRequest() {
+        try {
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String tmp = reader.readLine();
+            // Remove the terminating character '#'
+            tmp = tmp.substring(0, tmp.length() - 1);
+
+            request = tmp.split(" ");
+
+        } catch (IOException e) {
+            System.out.println("Socket Exception: " + e);
+        }
+    }
+
+    public void handleRequest() {
+
+        int num1 = Integer.parseInt(request[1]);
+        int num2 = Integer.parseInt(request[2]);
+
+        String operation = request[0];
+
+        if (operation.equals("+")) {
+            returnValue = String.valueOf(num1 + num2);
+        } else if (operation.equals("*")) {
+            returnValue = String.valueOf(num1 * num2);
+        }
+    }
+
+    public void returnResult() {
+
+        try {
+
+            OutputStream out = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out, true);
+
+            if (returnValue == null) {
+                returnValue = "An error occurred";
+            }
+
+            writer.println(returnValue);
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+
+
+    @Override
+    public void run() {
+
+        interpretRequest();
+        handleRequest();
+        returnResult();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
