10efe03301495ee767ddcb7dceaea42fa1f9741c
alexburnet <alexburnet03@gmail.com>
2024-03-19 17:10:26 +0000

    Initial Server Implementation

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,55 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    int serverPort = 6868;
+    ServerSocket socket;
+
+    Server() {
+
+        // Initialise socket
+
+        try {
+            socket = new ServerSocket(serverPort);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+
+    }
+
+    public void executeCucle() {
+
+        // Listen for connections, accept and start a new thread for each.
+
+        while (true) {
+            try {
+
+                Socket client = socket.accept();
+                Service s = new Service(client);
+                s.start();
+
+            } catch (IOException e) {
+                System.out.println("Exception: " + e);
+            }
+        }
+
+    }
+
+    public void shutdown() {
+        try {
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Server server = new Server();
+        server.executeCucle();
+    }
+
+
 }
